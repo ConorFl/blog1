d4c9a03d936f5cfe70e2a8c12bbc5e0f2f38c063
Apprentice <apprentice@devbootcamp.com>
2013-04-04 18:17:41 -0700

    proper associations

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -2,24 +2,14 @@ post '/new_post' do
   @new_post = Post.create(title: params[:title], content: params[:content])
   @posts_array = Post.all.reverse
   @tag_strings_array = params[:tags].split(',')
-  @tag_strings_array.each do |tag|
-    tag = Tag.find_or_create_by_word(tag)
-    @new_post.tags << tag
-    # @tag_id = Tag.find_by_word(tag).id
-    # PostTag.create(post_id: @new_post.id, tag_id: @tag_id)
+  @new_post.tags = @tag_strings_array.map do |word|
+    Tag.find_or_create_by_word(word)
   end
-  # @all_tags = []
-  # Tag.all.each do |tag|
-  #   @all_tags << tag[:word]
-  # end
   @every_tag = Tag.all
   erb :index
 end
 
 get '/post/:id' do
   @post = Post.find_by_id(params[:id])
-  # @associated_post_tags = @post.tags
-  # @tag_ids = @associated_post_tags.map{|p| p[:tag_id]}
-  # @associated_tags = Tag.find(@tag_ids).map{|x| x[:word]}
   erb :post
 end
